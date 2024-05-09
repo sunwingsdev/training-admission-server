a6313da83afb8bb71e92a924e5a57642ff01bd0e
simanto97 <saimumsimanto97@gmail.com>
2024-05-09 18:20:48 +0600

    delete api added

--- apis/admissionApi/admissionApi.js
+++ apis/admissionApi/admissionApi.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
 
 const admissionApi = (admissionCollection) => {
   const admissionRouter = express.Router();
@@ -6,7 +7,25 @@ const admissionApi = (admissionCollection) => {
   //   add admission list to db
   admissionRouter.post("/", async (req, res) => {
     const admissionInfo = req.body;
+    admissionInfo.createdAt = new Date();
     const result = await admissionCollection.insertOne(admissionInfo);
+    res.send(result);
+  });
+
+  // get all admission
+  admissionRouter.get("/", async (req, res) => {
+    const result = await admissionCollection
+      .find()
+      .sort({ createdAt: -1 })
+      .toArray();
+    res.send(result);
+  });
+
+  // delete a admission
+  admissionRouter.delete("/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await admissionCollection.deleteOne(query);
     res.send(result);
   });
 
